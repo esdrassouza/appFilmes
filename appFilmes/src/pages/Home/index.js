588f14d0831ce52ccc4570bfccc4647d91f03ce8
esdrassouza <esdrascsouza@hotmail.com>
2021-10-10 08:35:38 -0300

    Foi adicionado os diretorios Home e Movies

---
+++ appFilmes/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Pages} from 'react-native';
+
+function Home (){
+   return(
+        <View>
+            <Text>Pagina Home</Text>
+        </View>
+   );
+
+}
+
+export default Home;
---
+++ appFilmes/src/pages/Movies/index.js
@@ -0,0 +1,13 @@
+import  React from 'react';
+import {View, Text} from 'react-native';
+
+function Movies (){
+  return(
+    <View>
+        <Text>Pagina de Movies</Text>
+    </View>
+  );
+
+}
+
+export default Movies;
